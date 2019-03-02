5e3eec98bc7d4c177d87b5f2ade58c530b275844
SRIDHAR701 <sridhar.reddy15@yahoo.com>
2019-03-02 15:33:25 -0500

    cameraTest

--- cameraTest.py
+++ cameraTest.py
@@ -2,7 +2,7 @@ import cv2
 import io
 import os
 from threading import Thread
-import time
+import time, datetime
 
 from google.cloud import vision
 client = vision.ImageAnnotatorClient()
@@ -11,10 +11,24 @@ cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
 apiExec = time.time() * 1000
+fileExec = apiExec
 
-
+class Data_Point:
+	def __init__(self, timeStamp, anger, joy, suprise, sorrow):
+		self.timeStamp = timeStamp
+		self.anger = anger
+		self.joy = joy
+		self.suprise = suprise
+		self.sorrow = sorrow
+	
+global_Anger = 0
+global_Joy = 0
+global_suprise = 0
+global_sorrow = 0
+global_faceCount = 0
 
 def google_API(jpegFile):
+	global global_Anger, global_Joy, global_suprise, global_sorrow, global_faceCount
 	image = vision.types.Image(content=jpegFile)
 	response = client.face_detection(image=image)
 	faces = response.face_annotations
@@ -30,15 +44,47 @@ def google_API(jpegFile):
 		suprise = suprise + face.surprise_likelihood - 1
 		sorrow = sorrow + face.sorrow_likelihood - 1
 	print('Count: {}, Anger: {}, Joy: {}, Surprise: {}, Sorrow: {}'.format(facesCount, anger, joy, suprise, sorrow))
+	
+	if (facesCount != 0):
+		global_Anger = global_Anger + anger
+		global_Joy = global_Joy + joy
+		global_suprise = global_suprise + suprise
+		global_sorrow = global_sorrow + sorrow
+		global_faceCount = global_faceCount + facesCount
 	return 0
 
-	
+dataBase = []
 
+def file_Exec():
+	global dataBase, global_Anger, global_Joy, global_suprise, global_sorrow, global_faceCount
+	#Algorithm and update the database and clear and update globals
+	dataBase.append(Data_Point(datetime.datetime.now().strftime("%M:%S"), global_Anger/global_faceCount, \
+		global_Joy/global_faceCount, global_suprise/global_faceCount, global_sorrow/global_faceCount))
+		
+	global_Anger = 0
+	global_Joy = 0
+	global_suprise = 0
+	global_sorrow = 0
+	global_faceCount = 0
+	#Write database to file
+	file = open('output.txt','w')
+	
+	for object in dataBase:
+		file.write(object.timeStamp)
+		file.write(" ")
+	file.write("\n")
+	for object in dataBase:
+		file.write(str(object.joy))
+		file.write(" ")
+	file.close()
+	print('printed')
+	
+	
 if vc.isOpened():
 	rval, frame = vc.read()
 else:
 	rval = False
-	
+
 
 	
 while rval:
@@ -52,7 +98,10 @@ while rval:
 		t.start()
 		apiExec = time.time() * 1000
 	
-	
+	if((time.time() * 1000) - fileExec > 5000): #runs every 5000 ms
+		t1 = Thread(target=file_Exec)
+		t1.start()
+		fileExec = time.time() * 1000
 	
 	key = cv2.waitKey(20)
 	if key == 27:
